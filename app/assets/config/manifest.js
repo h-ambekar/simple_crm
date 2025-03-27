c72200e66955187e006e8a81492cbb1c5cb94431
Harsh Ambekar <hambekar@academic.rrc.ca>
2025-03-27 18:26:17 -0500

    Completed CRM challenge with ActiveAdmin, custom views, partials, routes, and asset configuration

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,6 @@
+//= link application.css
+//= link application.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
 //= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_directory ../javascripts .js
+//= link_tree ../../javascript/controllers .js
