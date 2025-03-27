853a9f7528702f7cb078f612518a9efd25039a55
Harsh Ambekar <hambekar@academic.rrc.ca>
2025-03-27 17:55:14 -0500

    Adding Admin Id password in the database

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,23 +1,13 @@
 ActiveAdmin.register Customer do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :full_name, :phone_number, :email_address, :notes
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:full_name, :phone_number, :email_address, :notes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  # Permit parameters for ActiveAdmin forms
+  permit_params :full_name, :phone_number, :email_address, :notes, :image
+
+  # Show page with image preview
   show do
     attributes_table do
       row :full_name
-      row :phone_numbera
+      row :phone_number
       row :email_address
       row :notes
       row :image do |customer|
@@ -29,4 +19,17 @@ ActiveAdmin.register Customer do
       end
     end
   end
-end  
+
+  # Form to upload customer data and image
+  form do |f|
+    f.inputs do
+      f.input :full_name
+      f.input :phone_number
+      f.input :email_address
+      f.input :notes
+      f.input :image, as: :file
+    end
+    f.actions
+  end
+
+end
